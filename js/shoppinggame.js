8da4cd4ab59c3607d6b2ebc23fb152f63612cde1
Brandao-lang <elijahbrandao@gmail.com>
2021-07-06 14:54:49 -0400

    module2

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -230,7 +230,11 @@ const findProductById = (id) => {
 };
 
 // Complete this function
-const generateProductId = () => { };
+const generateProductId = () => { 
+    let min = Math.ceil(1);
+    let max = Math.floor(20);
+    return Math.floor(Math.random() * (max - min + 1) + min)
+};
 
 
 const getProduct = (prodList, pId) => {
